262b27f151bdbdab7bc3d49acadefaf4d5e57c58
Janani123 <110513459+it21143786@users.noreply.github.com>
2022-08-10 11:19:28 +0000

    what did you change?

--- tute01.cpp
+++ tute01.cpp
@@ -18,13 +18,15 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 int main() 
-{
-    float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
-    inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+{ 
+  float cm,inches;
+  cout << "Enter the Length in cm" << endl;
+  cin >> cm;
+  inches = cm / 2.54;
+  cout << setiosflags(ios::fixed) << setprecision(2) << "cm = " << inches << " inches" << endl;
     return 0;
 }  
